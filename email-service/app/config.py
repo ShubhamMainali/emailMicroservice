17c7d2bee7eb06f57a21d9ea1e4f0f400ca13c4a
ShubhamMainali <shubhammainali09@gmail.com>
2020-08-25 01:44:05 +0530

    Update config.py

--- email-service/app/config.py
+++ email-service/app/config.py
@@ -4,10 +4,3 @@
 # USERNAME = "Mail account USERNAME"
 # PASSWORD = "Mail account PASSWORD"
 # SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@localhost/{}".format('Username','Password','DBname') #stagging_application
-
-
-SMTP_HOST = "mail.zonalsystems.com"
-SMTP_PORT = 587
-USERNAME = "zs-support"
-PASSWORD = "hZ8!dx7Wc#XZaMUN"
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:1@localhost/emails" #stagging_application
